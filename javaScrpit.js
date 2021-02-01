c3e9a7421fa126c59447b847d1532493c528ea78
Fernando Soares Neto <fernandonetosoares@hotmail.com>
2021-02-01 14:41:12 -0300

    Add two buttons

--- javaScrpit.js
+++ javaScrpit.js
@@ -2,5 +2,34 @@ function clicar() {
     Swal.fire('Menssagem Simples')
 }
 function erro(){
-    Swal.fire('Oops...','Ocorreu um erro!','ERRO')
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...', 
+        text: ' Ocorreu um erro inesperado',
+        footer:'Tente novamente'  
+    })
 }
+function pergunta(){
+    Swal.fire(
+        'Você Clicou?',
+        'Tem certeza?',
+        'question'
+    )
+}
+function salvar(){
+    Swal.fire({
+        title:'Você deseja salvar as mudanças?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: `Salvar`,
+        denyButtonText: `Não Salvar`,
+    }).then((result) => {
+        if(result.isConfirmed){
+            Swal.fire('Salvo!!','','success')
+        }else if (result.isDenied){
+            Swal.fire('As Mudanças não foram Salvas!','', 'info')
+
+        }
+    })
+}
+
